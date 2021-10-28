65b203351d88eac61c395e014b1e2762ce56bb6c
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-10-28 15:49:52 +0200

    modif du css et js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const navMenu = document.getElementById("nav-menu"),
   navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/* Valide si la constante existe */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
@@ -12,7 +12,7 @@ if (navToggle) {
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/* Valide si la constante existe */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
@@ -24,7 +24,8 @@ const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
+  // Quand on click sur chaque liens on remove le menu
+
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -43,7 +44,7 @@ let homeSwiper = new Swiper(".home-swiper", {
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  // Quand le scroll est supérieur à 50 viewport de hauteur
   if (this.scrollY >= 50) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
@@ -84,7 +85,7 @@ window.addEventListener("scroll", scrollActive);
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  // Quand le scroll est supérieur à 460h viewport,
   if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
 }
